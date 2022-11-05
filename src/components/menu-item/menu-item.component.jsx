54c5a418cc741a402f70e4f2a751386d4725c40b
Christian CHukwu <christianchukwu@MacBook-Pro.local>
2022-11-05 09:37:35 +0100

    broken homepage into smaller components, finished styling menu-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,9 +4,10 @@ import React from 'react'
 
 const MenuItem = ({title, imageUrl, size}) => {
   return (
-    <div style={{backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
-        <div className="comtent">
-          <h1 className="title">{title}</h1>
+    <div className={`${size} menu-item`}>
+      <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}} />
+        <div className="content">
+          <h1 className="title">{title.toUpperCase()}</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
     </div>
